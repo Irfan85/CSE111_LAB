a95174bdbcb4e14a22d6ac676bb5e08eba13c776
Chowdhury Mohammad Irfanuddin <irfan.muhit85@gmail.com>
2019-10-15 03:58:35 +0600

    Lab04 Task15 Done

--- src/Lab04/Task15/BoroInt.java
+++ src/Lab04/Task15/BoroInt.java
@@ -8,9 +8,31 @@ public class BoroInt {
     }
 
     public BoroInt(String val) {
+        //System.out.println("probably 1 was ok here: "+ val);
         String trimmedVal = trim(val);
         if(validValue(trimmedVal)){
-            this.val = trimmedVal;
+                int leadingZeroCount = 0;
+                for (int i = 0; i < trimmedVal.length(); i++) {
+                    if (trimmedVal.charAt(i) == '0') {
+                        leadingZeroCount++;
+                    } else {
+                        break;
+                    }
+                }
+
+                int newSize = trimmedVal.length() - leadingZeroCount;
+                char[] oldArray = trimmedVal.toCharArray();
+                char[] leadingZeroFreeArray = new char[newSize];
+                for (int i = 0; i < leadingZeroFreeArray.length; i++) {
+                    leadingZeroFreeArray[i] = oldArray[leadingZeroCount++];
+                }
+
+                if(leadingZeroFreeArray.length == 0){
+                    this.val = "0";
+                }else {
+                    this.val = new String(leadingZeroFreeArray);
+                }
+
         }else {
             System.err.println("Non Number Exception");
         }
@@ -36,22 +58,24 @@ public class BoroInt {
         for(int i = boroVal.length() - 1; i >= 0; i--){
             int top = Integer.parseInt(boroVal.charAt(i) + "");
             if(chotoValIndex < 0){
-                if((top + carry) != 10) {
+                //if((top + carry) >= 10) {
                     resVal = resVal.concat((top + carry) % 10 + "");
-                }else {
-                    resVal = resVal.concat((top + carry) % 10 + "");
-                }
-                carry = (top+carry) / 10;
+                    carry = (top+carry) / 10;
+//                }else {
+//                    resVal = resVal.concat((top + carry) % 10 + "");
+//                    carry = top / 10;
+//                }
             }else {
                 int bottom = Integer.parseInt(chotoVal.charAt(chotoValIndex--) + "");
                 int sum = top + bottom + carry;
-                if (sum >= 10) {
+                //if (sum >= 10) {
                     carry = sum / 10;
-                }
+                //}else{
+                    //carry = 0;
+                //}
                 resVal = resVal.concat(sum%10 + "");
             }
         }
-
         resVal = resVal.concat(carry + "");
         char[] resVal_chars = resVal.toCharArray();
         char[] reversedCharArray = new char[resVal_chars.length];
@@ -59,7 +83,6 @@ public class BoroInt {
         for(int i = resVal_chars.length - 1; i >= 0; i--){
             reversedCharArray[j++] = resVal_chars[i];
         }
-
         return new BoroInt(new String(reversedCharArray));
     }
 
@@ -75,14 +98,20 @@ public class BoroInt {
                 chotoVal = this.val;
             }
         }
-        System.out.println(boroVal + ", " + chotoVal);
+        //System.out.println(boroVal + ", " + chotoVal);
 
         int carry = 0, chotoValIndex = chotoVal.length() - 1;
 
         for(int i = boroVal.length() - 1; i >= 0; i--){
             int top = Integer.parseInt(boroVal.charAt(i) + "");
             if(chotoValIndex < 0){
-                resVal = resVal.concat((top%10 - carry)  + "");
+                if(top < carry){
+                    top = top + 10;
+                }
+
+                int sub = (top - carry)%10;
+                //System.out.println("SUB:" + sub + "=" + "TOP:" + top%10 + "+" + "CARRY:" + carry);
+                resVal = resVal.concat(sub  + "");
                 carry = top / 10;
             }else {
                 int bottom = Integer.parseInt(chotoVal.charAt(chotoValIndex--) + "");
@@ -92,12 +121,17 @@ public class BoroInt {
                 }
 
                 int sub = top - (bottom + carry);
-                //System.out.println(sub);
                 carry = top / 10;
+
+//                if (sub < 0){
+//                    System.err.println("######################################");
+//                }
 
                 resVal = resVal.concat(sub + "");
             }
         }
+
+        //System.out.println("RES VAL:"+resVal);
 
         char[] resVal_chars = resVal.toCharArray();
         char[] reversedCharArray = new char[resVal_chars.length];
@@ -106,7 +140,76 @@ public class BoroInt {
             reversedCharArray[j++] = resVal_chars[i];
         }
 
+        //System.out.println("SUBTRACT REVERSED:" + new String(reversedCharArray));
         return new BoroInt(new String(reversedCharArray));
+    }
+
+    public BoroInt multiply(BoroInt boroInt){
+        BoroInt result = new BoroInt();
+        String boroVal = this.val, chotoVal = boroInt.getVal();
+
+        if(this.val.length() < boroInt.getVal().length()){
+            boroVal = boroInt.getVal();
+            chotoVal = this.val;
+        }
+
+        for(int i = chotoVal.length()-1; i >=0; i--){
+            String resVal = "";
+            int bottom = Integer.parseInt(chotoVal.charAt(i)+"");
+            int carry = 0;
+            for(int j = boroVal.length()-1; j >= 0; j--){
+                int top = Integer.parseInt(boroVal.charAt(j)+"");
+                int mul = top*bottom + carry;
+                resVal = resVal.concat((mul%10)+"");
+                carry = mul/10;
+            }
+
+            resVal = resVal.concat(carry+"");
+
+            char[] resVal_chars = resVal.toCharArray();
+            char[] reversedCharArray = new char[resVal_chars.length];
+            int j = 0;
+            for(int k = resVal_chars.length - 1; k >= 0; k--){
+                reversedCharArray[j++] = resVal_chars[k];
+            }
+
+            String realString = new String(reversedCharArray);
+            for(int x = chotoVal.length() - 1; x > i; x--){
+                realString = realString.concat("0");
+            }
+
+            //System.out.println(realString);
+
+            result = result.add(new BoroInt(realString));
+            //System.out.println("SUMMATION:" + result.getVal());
+        }
+        return result;
+    }
+
+    public BoroInt divide(BoroInt boroInt){
+        BoroInt count = new BoroInt();
+        BoroInt divident = new BoroInt(this.val);
+
+        if(!boroInt.getVal().equals("0")) {
+            while(true){
+                //System.out.println(divident.getVal());
+                if(divident.getVal().length() < boroInt.getVal().length()){
+                    break;
+                }else if (divident.getVal().length() == boroInt.getVal().length()){
+                    if (Integer.parseInt(divident.getVal().charAt(0) + "") < Integer.parseInt(boroInt.getVal().charAt(0) + "")){
+                        break;
+                    }
+                }
+
+                divident = divident.subtract(boroInt);
+                //System.out.println(divident.getVal());
+                count = count.add(new BoroInt(1));
+            }
+
+        }else {
+            System.err.println("Divide by 0 Exception");
+        }
+        return count;
     }
 
     private boolean validValue(String val) {
--- src/Lab04/Task15/BoroIntDemo.java
+++ src/Lab04/Task15/BoroIntDemo.java
@@ -2,11 +2,11 @@ package Lab04.Task15;
 
 public class BoroIntDemo {
     public static void main(String[] args) {
-        BoroInt b1 = new BoroInt("99990");
-        BoroInt b2 = new BoroInt("9999");
-        BoroInt sum = b1.add(b2);
-        BoroInt sub = b1.subtract(b2);
-        System.out.println(sum.getVal());
-        System.out.println(sub.getVal());
+        BoroInt b1 = new BoroInt("22");
+        BoroInt b2 = new BoroInt("7");
+        BoroInt mul = b1.divide(b2);
+        //BoroInt sub = b1.subtract(b2);
+        System.out.println(mul.getVal());
+        //System.out.println(sub.getVal());
     }
 }
