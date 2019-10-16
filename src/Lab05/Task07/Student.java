797d233434f2f0ce5e3cc4019df813946d7a454d
Chowdhury Mohammad Irfanuddin <irfan.muhit85@gmail.com>
2019-10-17 01:06:18 +0600

    Lab05 Complete

---
+++ src/Lab05/Task07/Student.java
@@ -0,0 +1,20 @@
+package Lab05.Task07;
+
+public class Student {
+    public static int numberOfStudents = 0;
+    private String name;
+
+    public Student(){
+        this.name = "Default Student";
+        numberOfStudents++;
+    }
+
+    public Student(String name){
+        this.name = name;
+        numberOfStudents++;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/Lab05/Task10/Student.java
@@ -0,0 +1,11 @@
+package Lab05.Task10;
+
+public class Student {
+    public String name;
+    public int id;
+
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+}
