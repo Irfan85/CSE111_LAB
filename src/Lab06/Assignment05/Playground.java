3fd6d7a8e4e74741a75277cc2f0180cc1f3a7a46
Chowdhury Mohammad Irfanuddin <irfan.muhit85@gmail.com>
2019-10-31 02:20:43 +0600

    Lab06 Complete

---
+++ src/Lab06/Assignment05/Playground.java
@@ -0,0 +1,10 @@
+package Lab06.Assignment05;
+
+public class Playground {
+    public static void main(String[] args) {
+        String s = "abc";
+        String s1 = "bcd";
+        System.out.println(s.compareTo(s1));
+        System.out.println(s1.compareTo(s));
+    }
+}
